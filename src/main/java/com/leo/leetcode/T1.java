d0625a94daa7edd07771567c7372d1fe0998a2d5
leo <linxiaotao1993@vip.qq.com>
2018-09-20 10:38:59 +0800

    [LeetCode] 优化 T1 解法

--- src/main/java/com/leo/leetcode/T1.java
+++ src/main/java/com/leo/leetcode/T1.java
@@ -8,88 +8,17 @@ import java.util.Map;
  */
 public class T1 {
 
-    static int[] twoSum(int[] nums, int target) {
-        final Map<Integer, String> map = new HashMap<>(nums.length);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int value = nums[i];
-            String insert = map.get(value);
-            if (insert == null) {
-                insert = String.valueOf(i);
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
             } else {
-                insert += "," + String.valueOf(i);
-            }
-
-            map.put(value, insert);
-        }
-        // 快排
-        quickSort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            int value = nums[i];
-            // 二分查找
-            String sourceIndex = map.get(value);
-            int firstIndex = Integer.parseInt(sourceIndex.split(",")[0]);
-            int searchIndex = binarySearch(nums, i, target - value);
-            if (searchIndex > -1) {
-                sourceIndex = map.get(nums[searchIndex]);
-                for (String index : sourceIndex.split(",")) {
-                    if (Integer.parseInt(index) != firstIndex) {
-                        return new int[]{firstIndex, Integer.parseInt(index)};
-                    }
-                }
-
+                map.put(target - nums[i], i);
             }
         }
-        return new int[2];
+        return new int[]{};
     }
 
-    private static void quickSort(int[] nums) {
-        if (nums.length == 0) {
-            return;
-        }
-        sort(nums, 0, nums.length - 1);
-
-    }
-
-    private static void sort(int[] nums, int start, int end) {
-        if (start < end) {
-            int mid = getMid(nums, start, end);
-            sort(nums, start, mid - 1);
-            sort(nums, mid + 1, end);
-        }
-    }
-
-    private static int getMid(int[] nums, int start, int end) {
-        int temp = nums[start];
-        while (start < end) {
-            while (start < end && nums[end] >= temp) {
-                end--;
-            }
-            nums[start] = nums[end];
-            while (start < end && nums[start] < temp) {
-                start++;
-            }
-            nums[end] = nums[start];
-        }
-        nums[start] = temp;
-        return start;
-    }
-
-    private static int binarySearch(int[] nums, int start, int target) {
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = (start + end) >>> 1;
-            int midVal = nums[mid];
-            if (target > midVal) {
-                start = mid + 1;
-            } else if (target < midVal) {
-                end = mid - 1;
-            } else {
-                // find
-                return mid;
-            }
-        }
-        // not find
-        return -1;
-    }
 
 }
--- src/test/java/com/leo/leetcode/T1Test.java
+++ src/test/java/com/leo/leetcode/T1Test.java
@@ -10,19 +10,18 @@ public class T1Test {
     @Test
     public void twoSum() {
 
+        T1 t1 = new T1();
+
         int[] nums = new int[]{-3, 4, 3, 90};
-        int[] result = T1.twoSum(nums, 0);
-        System.out.println("result: " + Arrays.toString(result));
+        int[] result = t1.twoSum(nums, 0);
         Assert.assertArrayEquals(result, new int[]{0, 2});
 
         nums = new int[]{3, 3};
-        result = T1.twoSum(nums, 6);
-        System.out.println("result: " + Arrays.toString(result));
+        result = t1.twoSum(nums, 6);
         Assert.assertArrayEquals(result, new int[]{0, 1});
 
         nums = new int[]{3,2,4};
-        result = T1.twoSum(nums, 6);
-        System.out.println("result: " + Arrays.toString(result));
+        result = t1.twoSum(nums, 6);
         Assert.assertArrayEquals(result, new int[]{1,2});
     }
 }
